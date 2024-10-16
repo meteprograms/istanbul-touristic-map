5f71874b973499fdca6146f498705f9e22040f97
Ahmet Mete <ahmetmek97@zedat.fu-berlin.de>
2024-10-16 17:58:06 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,257 @@
+import folium
+
+map = folium.Map(location=(41.0082, 28.9784), zoom_start=14)
+
+folium.Marker(
+    location=[41.011574, 28.983269],
+    tooltip="Click to reveal more!",
+    popup="TOPKAPI PALACE, Topkapi Palace is a grand Ottoman royal residence in Istanbul, known for its stunning architecture and historical significance. ",
+    icon=folium.Icon(color="blue"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0084, 28.9802],
+    tooltip="Click to reveal more!",
+    popup="HAGIA SOPHIA, Hagia Sophia is a monumental structure in Istanbul, originally a Byzantine cathedral, later an Ottoman mosque, and now a museum-turned-mosque, renowned for its massive dome and rich history. ",
+    icon=folium.Icon(color="blue"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0057, 28.9771],
+    tooltip="Click to reveal more!",
+    popup="THE BLUE MOSQUE, The Blue Mosque, also known as Sultan Ahmed Mosque, is a historic mosque in Istanbul famous for its stunning blue-tiled interior and six towering minarets. ",
+    icon=folium.Icon(color="blue"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0133, 28.9815],
+    tooltip="Click to reveal more!",
+    popup="GÜLHANE PARK, Gülhane Park is a historic urban park in Istanbul, located near Topkapi Palace, known for its beautiful gardens, scenic views, and tranquil atmosphere. ",
+    icon=folium.Icon(color="green"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0071, 28.9767],
+    tooltip="Click to reveal more!",
+    popup="THE GERMAN FOUNTAIN, The Alman Çeşmesi, or German Fountain, is a historic gazebo-style fountain in Istanbul, gifted by German Emperor Wilhelm II in 1901, known for its neo-Byzantine design and decorative mosaics. ",
+    icon=folium.Icon(color="beige"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0054, 28.9748],
+    tooltip="Click to reveal more!",
+    popup="THE WALLED OBELISK, The Walled Obelisk is an ancient stone monument in Istanbul's Hippodrome, originally built during the Byzantine era, characterized by its rough stone construction and once topped with gilded bronze plaques.",
+    icon=folium.Icon(color="beige"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0083, 28.9777],
+    tooltip="Click to reveal more!",
+    popup="BASILLICA CISTERN, The Basilica Cistern is a vast underground water reservoir in Istanbul, built during the Byzantine era, famous for its impressive columns, vaulted ceilings, and eerie, atmospheric lighting. ",
+    icon=folium.Icon(color="blue"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0106, 28.9680],
+    tooltip="Click to reveal more!",
+    popup="GRAND BAZAAR, One of the largest and oldest covered markets in the world, known for its maze-like streets filled with shops selling jewelry, textiles, spices, and more. ",
+    icon=folium.Icon(color="lightred"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0165, 28.9705],
+    tooltip="Click to reveal more!",
+    popup="EGYTPIAN BAZAAR, Also known as the Spice Bazaar, it is famous for its vibrant stalls selling spices, sweets, dried fruits, and traditional Turkish products.",
+    icon=folium.Icon(color="lightred"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0117, 28.9813],
+    tooltip="Click to reveal more!",
+    popup="ISTANBUL ARCHAELOGICAL MUSEUM, The Istanbul Archaeological Museum is a major museum complex in Istanbul, housing a vast collection of artifacts from various civilizations, including ancient Greek, Roman, and Byzantine treasures.",
+    icon=folium.Icon(color="red"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0391, 29.0003],
+    tooltip="Click to reveal more!",
+    popup="DOLMABAHCE PALACE, An opulent palace along the Bosphorus, known for its stunning architecture and beautiful gardens, serving as the main administrative center of the Ottoman Empire in the 19th century.",
+    icon=folium.Icon(color="blue"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0256, 28.9741],
+    tooltip="Click to reveal more!",
+    popup="GALATA TOWER, A medieval stone tower offering panoramic views of Istanbul, originally built as a watchtower in the 14th century.",
+    icon=folium.Icon(color="blue"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0160, 28.9640],
+    tooltip="Click to reveal more!",
+    popup="SULEYMANIYE MOSQUE, An iconic mosque designed by the architect Mimar Sinan in the 16th century, noted for its grand dome, beautiful gardens, and stunning interior.",
+    icon=folium.Icon(color="blue"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0211, 29.0041],
+    tooltip="Click to reveal more!",
+    popup="MAIDENS TOWER, A historic tower located on a small island in the Bosphorus, offering a café and stunning views of the city.",
+    icon=folium.Icon(color="blue"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0370, 28.9851],
+    tooltip="Click to reveal more!",
+    popup="TAKSIM SQUARE, A major public space in Istanbul, known for its vibrant atmosphere, restaurants, shops, and the Republic Monument.",
+    icon=folium.Icon(color="beige"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0341, 28.9788],
+    tooltip="Click to reveal more!",
+    popup="ISTIKLAL STREET, A bustling pedestrian avenue lined with shops, cafes, and historic buildings, connecting Taksim Square to the Galata Tower.",
+    icon=folium.Icon(color="beige"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0169, 28.9722],
+    tooltip="Click to reveal more!",
+    popup="YENI CAMII, An iconic mosque located at the entrance of the Spice Bazaar, known for its stunning architecture and beautiful interior.",
+    icon=folium.Icon(color="blue"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0311, 28.9736],
+    tooltip="Click to reveal more!",
+    popup="PERA PALACE HOTEL, A historic luxury hotel that opened in 1895, famous for its elegant architecture and association with notable guests like Agatha Christie.",
+    icon=folium.Icon(color="blue"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0228, 28.9739],
+    tooltip="Click to reveal more!",
+    popup="TUNNEL, A historic funicular railway that connects Karaköy to Beyoğlu, one of the oldest underground railways in the world, providing easy access to Istiklal Street.",
+    icon=folium.Icon(color="beige"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0312, 28.9392],
+    tooltip="Click to reveal more!",
+    popup="CHORA CHURCH: A Byzantine church known for its stunning mosaics and frescoes, showcasing some of the finest examples of Byzantine art.",
+    icon=folium.Icon(color="blue"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0160, 28.9556],
+    tooltip="Click to reveal more!",
+    popup="VALENS AQUEDUCT: An ancient aqueduct built by the Romans in the 4th century, which once supplied water to the city and is a prominent historical landmark.",
+    icon=folium.Icon(color="beige"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0259, 28.9828],
+    tooltip="Click to reveal more!",
+    popup="ISTANBUL MODERN MUSEUM: A contemporary art museum showcasing modern and contemporary Turkish and international art, located along the Bosphorus.",
+    icon=folium.Icon(color="red"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0075, 28.9744],
+    tooltip="Click to reveal more!",
+    popup="1001 DIREK CISTERN: An ancient underground cistern built in the 6th century during the Byzantine Empire, famous for its impressive columns and serene atmosphere.",
+    icon=folium.Icon(color="blue"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0085, 28.9713],
+    tooltip="Click to reveal more!",
+    popup="CEMBERLI TAS: Also known as the Column of Constantine, a historic column located near the Grand Bazaar, originally erected by Emperor Constantine the Great in the 4th century as a monument to Christianity.",
+    icon=folium.Icon(color="beige"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0500, 29.0116],
+    tooltip="Click to reveal more!",
+    popup="YILDIZ PALACE: A former Ottoman palace situated in Yıldız Park, known for its beautiful gardens and architectural styles, used as a royal residence in the 19th century.",
+    icon=folium.Icon(color="blue"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0440, 29.0161],
+    tooltip="Click to reveal more!",
+    popup="CIRAGAN PALACE: A luxurious palace on the Bosphorus, built in the 19th century, now serving as a hotel, renowned for its stunning architecture and scenic views.",
+    icon=folium.Icon(color="blue"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0819, 29.0669],
+    tooltip="Click to reveal more!",
+    popup="ANADOLU HISARI:  A historic fortress located on the Asian side of the Bosphorus, built by the Ottomans in the 14th century, offering beautiful views of the strait and surrounding areas.",
+    icon=folium.Icon(color="blue"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0848, 29.0567],
+    tooltip="Click to reveal more!",
+    popup="RUMELI HISARI: A well-preserved fortress on the European side of the Bosphorus, built in the 15th century by Sultan Mehmed II, known for its impressive walls and historical significance.",
+    icon=folium.Icon(color="blue"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0323, 28.9771],
+    tooltip="Click to reveal more!",
+    popup="ST. ANTOINE CHURCH: The largest Catholic church in Istanbul, known for its neo-Gothic architecture and beautiful stained glass windows, located in the Beyoğlu district.",
+    icon=folium.Icon(color="blue"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0426, 29.0399],
+    tooltip="Click to reveal more!",
+    popup="BEYLERBEYI SARAYI: A lavish summer palace on the Asian side of the Bosphorus, built in the 19th century, known for its stunning architecture, beautiful gardens, and exquisite interior decor.",
+    icon=folium.Icon(color="blue"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0302, 28.9757],
+    tooltip="Click to reveal more!",
+    popup="CHURCH OF ST. MARY DRAPERIS: An Armenian church located in the Beyoğlu district, known for its beautiful architecture and significant role in the Armenian community of Istanbul.",
+    icon=folium.Icon(color="blue"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0338, 28.9405],
+    tooltip="Click to reveal more!",
+    popup="PALACE OF THE PORPHYROGENITUS: An ancient Byzantine palace complex situated near the Byzantine walls, known for its historical significance and connection to the Byzantine emperors.",
+    icon=folium.Icon(color="blue"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0096, 28.9812],
+    tooltip="Click to reveal more!",
+    popup="HAGIA IRENE: An early Byzantine church located near Hagia Sophia, known for its beautiful architecture and as one of the few churches that was never converted into a mosque.",
+    icon=folium.Icon(color="blue"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0120, 28.9812],
+    tooltip="Click to reveal more!",
+    popup="TILED PAVILION MUSEUM: A historical pavilion located in the grounds of Topkapi Palace, showcasing exquisite tiles and ceramics from the Ottoman period.",
+    icon=folium.Icon(color="red"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0113, 28.9803],
+    tooltip="Click to reveal more!",
+    popup="MUSEUM OF THE ANCIENT ORIENT: A museum that houses an extensive collection of artifacts from ancient Near Eastern civilizations, including sculptures, pottery, and inscriptions.",
+    icon=folium.Icon(color="red"),
+).add_to(map)
+
+folium.Marker(
+    location=[41.0197, 28.9572],
+    tooltip="Click to reveal more!",
+    popup="ZEYREK MOSQUE: A historic mosque in Istanbul, originally built as the Church of the Pantokrator in the 12th century during the Byzantine era. After the Ottoman conquest, it was converted into a mosque and is renowned for its stunning architecture, beautiful mosaics, and significant two-story design, reflecting the rich cultural and religious history of the city.",
+    icon=folium.Icon(color="blue"),
+).add_to(map)
+
+map.show_in_browser()
